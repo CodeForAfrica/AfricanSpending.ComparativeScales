94edc6d922e8410817f1c42828e20fbcda931db6
uf0 <scnepa222@gmail.com>
2015-08-03 23:47:12 +0200

    bootstrap project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name comparativescalesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the comparativescalesApp
+ */
+angular.module('comparativescalesApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name comparativescalesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the comparativescalesApp
+ */
+angular.module('comparativescalesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
